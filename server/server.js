2a92ec028a8bfbcb96c609ae71f912a2af9cb37d
Rishi Kolasani <rkolasani95@gmail.com>
2018-08-22 11:56:42 -0400

    add connect and disconnect socket

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const express = require('express');
-
+const socketIO=require('socket.io');
 const path = require('path');
+const http = require('http');
 
 const publicPath = path.join(__dirname,'/../public');
 const port = process.env.PORT ||3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+  console.log('new user connected');
+  socket.on('disconnect',()=>{
+    console.log('disconnected  from the server');
+  });
+});
 
-app.listen(port,()=>{
-  console.log(`srver is up on 3000 ${port}`);
+
+
+server.listen(port,()=>{
+  console.log(`srver is up on  ${port}`);
 });
